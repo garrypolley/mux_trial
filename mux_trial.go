4d99da7df04d224e87523dede446440c0538a63d
Garry Polley <garry.polley@c2fo.com>
2016-02-24 22:58:35 -0600

    attempt future configuration and better handle repeat functions

--- mux_trial.go
+++ mux_trial.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -10,21 +11,30 @@ import (
 	"github.com/gorilla/mux"
 )
 
+const (
+	port    = 8080
+	timeout = 5
+)
+
+func handleFunc(r *mux.Router, url string, handler func(http.ResponseWriter, *http.Request)) {
+	r.HandleFunc(url, handlers.LogRequest(handler))
+}
+
 func main() {
 	r := mux.NewRouter().StrictSlash(true)
 
-	r.HandleFunc("/", handlers.LogRequest(handlers.HomeHandler))
-	r.HandleFunc("/products", handlers.LogRequest(handlers.ProductsHandler))
-	r.HandleFunc("/products/{id:[0-9]+}", handlers.LogRequest(handlers.ProductsIdHandler))
-	r.HandleFunc("/articles", handlers.LogRequest(handlers.ArticlesHandler))
+	handleFunc(r, "/", handlers.HomeHandler)
+	handleFunc(r, "/products", handlers.ProductsHandler)
+	handleFunc(r, "/products/{id:[0-9]+}", handlers.ProductsIdHandler)
+	handleFunc(r, "/articles", handlers.ArticlesHandler)
 
 	server := http.Server{
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf(":%d", port),
 		Handler:      r,
-		ReadTimeout:  time.Minute * 5,
-		WriteTimeout: time.Minute * 5,
+		ReadTimeout:  time.Minute * timeout,
+		WriteTimeout: time.Minute * timeout,
 	}
 
-	log.Printf("Starting server on :%d", 8080)
+	log.Printf("Starting server on :%d with a timeout of %d minutes", port, timeout)
 	server.ListenAndServe()
 }
